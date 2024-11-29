d79b88b642d40dcad378bd17600eacb17a2bc995
emilhet <elsa.milhet@etud.univ-jfc.fr>
2024-11-29 17:26:35 +0100

    ajout du code

--- src/main/java/bowling/PartieMonoJoueur.java
+++ src/main/java/bowling/PartieMonoJoueur.java
@@ -2,19 +2,27 @@ package bowling;
 
 /**
  * Cette classe a pour but d'enregistrer le nombre de quilles abattues lors des
- * lancers successifs d'<b>un seul et même</b> joueur, et de calculer le score
+ * lancés successifs d'<b>un seul et même</b> joueur, et de calculer le score
  * final de ce joueur
  */
 public class PartieMonoJoueur {
+
+	private final Tour premierTour; // Le premier tour
+	private Tour tourCourant; // Le tour en cours
 
 	/**
 	 * Constructeur
 	 */
 	public PartieMonoJoueur() {
-		for (int i = 0; i < 9; i++) {
-			tours[i] = new Tour();
+		// On initialise les 10 tours, en commençant par le dernier
+		// Chaque tour (sauf le dernier) est initialisé avec son tour suivant
+		// On construit ainsi une "liste chainée" de tours
+		tourCourant = new DernierTour();
+		for (int numero = 9; numero > 0; numero--) {
+			Tour nouveauTour = new Tour(numero, tourCourant);
+			tourCourant = nouveauTour;
 		}
-		tours[tours.length - 1] = new DernierTour();
+		premierTour = tourCourant;
 	}
 
 	/**
@@ -22,74 +30,50 @@ public class PartieMonoJoueur {
 	 *
 	 * @param nombreDeQuillesAbattues le nombre de quilles abattues lors de ce lancer
 	 * @throws IllegalStateException si la partie est terminée
-	 * @return vrai si le joueur doit lancer à nouveau pour continuer son tour, faux sinon	
+	 * @return vrai si le joueur doit lancer à nouveau pour continuer son tour, faux sinon
 	 */
 	public boolean enregistreLancer(int nombreDeQuillesAbattues) {
-		int currentTour = this.numeroTourCourant() - 1;
-		if (this.numeroTourCourant() <= 0) {
-			throw new IllegalStateException("can't enregistreLancer() to a finished game");
+		if (tourCourant == null)
+			throw new IllegalStateException("Le jeu est fini");
+
+		tourCourant.enregistreLancer(nombreDeQuillesAbattues);
+		if (tourCourant.estTermine()) {
+			tourCourant = tourCourant.next();
 		}
-		tours[this.numeroTourCourant() - 1].addScoreLancer(nombreDeQuillesAbattues);
-		if (tours[currentTour].getNumLancer() == 0) {
-			return false;
-		}
-		return true;
+		return ! (this.estTerminee() || tourCourant.getBoulesLancees() == 0);
 	}
 
 	/**
 	 * Cette méthode donne le score du joueur.
 	 * Si la partie n'est pas terminée, on considère que les lancers restants
 	 * abattent 0 quille.
+	 *
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		Integer[] score = new Integer[10];
-		Arrays.fill(score, 0);
-		for (int i = 0; i <= tours.length - 2; i++) {
-			if (i < 8 && tours[i + 1].getScoreQuilleLancer2() == -1) {
-				score[i] = tours[i].getScoreTour(tours[i + 1].getLancer1(), tours[i + 2].getLancer1());
-			} else {
-				score[i] = tours[i].getScoreTour(tours[i + 1].getLancer1(), tours[i + 1].getLancer2());
-			}
-			score[tours.length - 1] = tours[tours.length - 1].getScoreTour(null, null);
-		}
-		int scoreTot = 0;
-		for (int s : score) {
-			scoreTot += s;
-		}
-		return scoreTot;
+		return premierTour.score();
 	}
-
 
 	/**
-	 * @return vrai si la partie est terminée pour ce joueur, faux sinon
+	 * @return vrai si le jeu est finin faux sinon
 	 */
 	public boolean estTerminee() {
-		if (this.numeroTourCourant() == 0) {
-			return true;
-		}
-		return false;
+		return (tourCourant == null);
 	}
-
 
 	/**
 	 * @return Le numéro du tour courant [1..10], ou 0 si le jeu est fini
 	 */
 	public int numeroTourCourant() {
-		for (int i = 0; i < tours.length; i++) {
-			if (tours[i].getNumLancer() != 0) {
-				return i + 1;
-			}
-		}
-		return 0;
+		return estTerminee() ? 0 : tourCourant.getNumero();
 	}
 
 	/**
-	 * @return Le numéro du prochain lancer pour tour courant [1..3], ou 0 si le jeu
+	 * @return Le numéro du prochain lancer pour tour courant [1..2], ou 0 si le jeu
 	 *         est fini
 	 */
 	public int numeroProchainLancer() {
-		return tours[numeroTourCourant() - 1].getNumLancer();
+		return estTerminee() ? 0 : tourCourant.getBoulesLancees() + 1;
 	}
 
 }
